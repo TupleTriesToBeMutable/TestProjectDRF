ea4dbb70811d347e89712f7497ebeb8b610cdd5a
TupleSosok <jokylionplay@gmail.com>
2024-08-12 20:34:14 +0300

    WebApp

--- TestProject/urls.py
+++ TestProject/urls.py
@@ -16,7 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+     path('webapp/', include('webapp.urls')),
+]
+
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='/webapp/', permanent=True)),
 ]
